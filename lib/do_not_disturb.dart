c70ef2c3fa7cc7135c6d8518f600191f1a38de55
Mubarak Ibrahim <40618838+codeswot@users.noreply.github.com>
2023-07-12 08:51:41 +0100

    update example to use from interface

--- lib/do_not_disturb.dart
+++ lib/do_not_disturb.dart
@@ -1,11 +1,12 @@
 import 'do_not_disturb_platform_interface.dart';
 
 class DoNotDisturb {
-  Future<String?> getPlatformVersion() {
-    return DoNotDisturbPlatform.instance.getPlatformVersion();
-  }
-
-  Future<bool> setEnabled(bool enabled) {
+  Future<bool> setStatus(bool enabled) {
     return DoNotDisturbPlatform.instance.setStatus(enabled);
   }
+
+  Future<bool> get status async => await DoNotDisturbPlatform.instance.status;
+
+  Stream<bool> get statusAsStream =>
+      DoNotDisturbPlatform.instance.statusStream();
 }
